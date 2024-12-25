d0e95af523f54ed9d042b805f441bb4f59a56234
Yusra Ateeq <yusraateeq112@gmail.com>
2024-12-25 14:13:32 +0500

    commit

--- sanity-blog/next.config.ts
+++ sanity-blog/next.config.ts
@@ -1,9 +1,29 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ['cdn.sanity.io', 'images.twinkl.co.uk'], // Allow images from Sanity's CDN
+    
+//   },
+// };
+
+// export default nextConfig;
+
+
+
+
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     domains: ['cdn.sanity.io', 'images.twinkl.co.uk'], // Allow images from Sanity's CDN
-    
+  },
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
   },
 };
 
